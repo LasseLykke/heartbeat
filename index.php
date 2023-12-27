7a7e0996a83134ceffc38209f9dea3e42dd16d9f
Lasse Lykke <mail@lasselykke.com>
2023-12-27 14:24:12 +0100

    added index/login screen

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>H E A R T B E A T</title>
+</head>
+<body>
+    <h1>Heatbeat</h1>
+</body>
+</html>

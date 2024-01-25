b1cd5e5535a70194816008f938980f4a5a2105f2
Lasse Lykke <mail@lasselykke.com>
2024-01-25 21:01:38 +0100

    styling of form

--- header.php
+++ header.php
@@ -9,14 +9,14 @@ include 'connection.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <!--Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@400;700&display=swap" rel="stylesheet">
     <!--sub folders-->
     <link href="./style/base.css" type="text/css" rel="stylesheet">
     <link href="./style/layout.css" type="text/css" rel="stylesheet">
     <link href="./style/typo.css" type="text/css" rel="stylesheet">
-    <meta name="keywords" content="internal tooling for ordermanagement system." />
-    <meta name="description" content="Internal tooling for ordremanagement system and database" />
+    <meta name="keywords" content="Privat database for loggin workout and other stuff" />
+    <meta name="description" content="Privat database for loggin workout and other stuff" />
     <meta name="author" content="Lasse Lykke @ Lasselykke.com" />
 </head>

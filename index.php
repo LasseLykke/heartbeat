2cd0a2df9772aaf4e59350234f6fe43580c25119
Lasse Lykke <mail@lasselykke.com>
2024-02-07 20:55:10 +0100

    added alot of things to the front portfolio

--- index.php
+++ index.php
@@ -7,13 +7,40 @@ include 'header.php';
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <link href="./style/front.css" type="text/css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;400;1000&display=swap" rel="stylesheet">
         <title>lasselykke DOT com </title>
     </head>
     <body>
-        <div class="frontWrapper">
-            <h1>... under construction 👷 </h1>
-            <p style="font-style: italic"> in the meantime you can visit my <a href="https://github.com/LasseLykke" class="git" target="_blank">github</a> </p>
-        </div>
+        <div class="wrapper">
+            <div class="splashContent">
+            <h1>Hello</h1><br>
+            <h1>I'm Lasse Lykke 👋🏻</h1><br>
+            <p>My name is Lasse Lykke, I'm based in the southern part of Zealand, Denmark.
+    By day, I work as a Sales Assistent in Holm Foto Aps. Here we make everything from printing images, do analog film developing, framing and digitizing video tape, negativs and so much more.
+
+    Technology has always been an important core of my interests. From an early age, I loved being able to use IT to network with people around the world and build something that others would benefit from.
+    I spend all my free time on web development and design. In order to constantly get better, I always taking a course or creating a website.</p>
+            </div>
+
+            <div class="projects">
+                <h1>Projects :</h1>
+                <br>
+                <h3>Heartbeat</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita, aspernatur id quos natus quo et itaque optio delectus voluptas officia porro autem, odit minima aut officiis, repellat at ullam.</p>
+
+                <h3>Holmfoto.dk</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita, aspernatur id quos natus quo et itaque optio delectus voluptas officia porro autem, odit minima aut officiis, repellat at ullam.</p>
+
+                <h3>Donnés</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita, aspernatur id quos natus quo et itaque optio delectus voluptas officia porro autem, odit minima aut officiis, repellat at ullam.</p>
+
+                <h3>Salongitte.dk</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita, aspernatur id quos natus quo et itaque optio delectus voluptas officia porro autem, odit minima aut officiis, repellat at ullam.</p>
+            </div>
+</div>
     </body>
 </html>

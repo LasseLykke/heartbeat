9e9d939bd17e9cd23aaa007fd254346a3405fcde
Lasse Lykke <mail@lasselykke.com>
2024-02-18 16:03:41 +0100

    added counter til carts

--- forside.php
+++ forside.php
@@ -5,12 +5,6 @@ session_start();
 if (isset($_SESSION['user_id']) && isset($_SESSION['user_name'])) {
 
     include 'header.php';
-
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        $workoutID = htmlspecialchars($_POST["workoutID"]); 
-    } 
-
-    $_SESSION['workoutID'] = $workoutID;
     ?>
 <!DOCTYPE html>
     <html>
@@ -40,34 +34,57 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['user_name'])) {
         
         
         <div class="dataContainer">
+
+        <?php 
+        // Step 2: Execute SQL Query
+        $sql1 = "SELECT workoutID FROM workout";
+        $result1 = $conn->query($sql1);
+
+        $sql2 = "SELECT COUNT(*) AS num_rows FROM pain WHERE painState = 'Ja'";
+        $result2 = $conn->query($sql2);
+        ?>
+    
+        
         <h2 class="formHeader">Data Summary:</h2>
         
         <div class="cartWrapper">
             <div class="cart">
                 <h3>Workouts:</h3>
+                <a href="træningsform.php"><button class="cartBtn">
+                <h1 class="test">
+                    <?php 
+                    if ($result1->num_rows > 0) {
+                        // Fetch the first row
+                            $row1 = $result1->fetch_assoc();
+                            echo "<div id='cartBtn'>";
+                            echo "<h3>" . $row1["workoutID"] . "</h3>";
+                            echo "</div>";
+                        } else {
+                            echo "0";
+                        }
+                    ?>
+                </h1>
+                </button></a>
+            </div>
+
+            <div class="cart">
+                <h3>Pain:</h3>
                 <a href="painForm.php"><button class="cartBtn">
                 <h1 class="test">
                     <?php 
-                    echo $_SESSION['workoutID'];
+                    if ($result2->num_rows > 0) {
+                        $row2 = $result2->fetch_assoc();
+                        $count = $row2['num_rows'];
+                    
+                        // Display the count in HTML
+                        echo "<div id='cart'>";
+                        echo "<h3>" . $count . "</h3>";
+                        echo "</div>";
+                    } else {
+                        echo "Ingen resultater fundet";
+                    }
                     ?>
                 </h1>
-                <p>-></p>
-                </button></a>
-            </div>
-
-            <div class="cart">
-                <h3>Workouts:</h3>
-                <a href="painForm.php"><button class="cartBtn">
-                <h1 class="test">55</h1>
-                <p>-></p>
-                </button></a>
-            </div>
-
-            <div class="cart">
-                <h3>Workouts:</h3>
-                <a href="painForm.php"><button class="cartBtn">
-                <h1 class="test">55</h1>
-                <p>-></p>
                 </button></a>
             </div>
         </div>
